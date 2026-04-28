066759b2c0e76c983bfbc60395c3dc3c937e9f26
pdhruv22-hue <pdhruv22@gmail.com>
2026-04-28 08:45:19 -0400

    Update accountability-data.js 2026-04-28

---
+++ accountability-data.js
@@ -0,0 +1,28 @@
+window.ACCOUNTABILITY_DATA = {
+  "generated_at": "2026-04-28T06:00:30.948837",
+  "week_start": "2026-04-27",
+  "week_end": "2026-05-03",
+  "scores": {
+    "cre_reviewed": {
+      "count": 0,
+      "target": 5,
+      "days": []
+    },
+    "deep_work": {
+      "count": 0,
+      "target": 5,
+      "days": []
+    },
+    "amda_boundary": {
+      "count": 0,
+      "target": 7,
+      "days": []
+    },
+    "income_played": {
+      "count": 0,
+      "target": 1,
+      "days": []
+    }
+  },
+  "log": []
+};
