89b3b47c4eacd0faa463fa201f32c81c06c73377
pdhruv22-hue <pdhruv22@gmail.com>
2026-04-30 20:26:16 -0400

    Data update 2026-04-30

--- deploy-data.js
+++ deploy-data.js
@@ -1,1 +1,1 @@
-window.DEPLOY_DATA = {"latest":null};
+window.DEPLOY_DATA = {"empty": true, "placeholder": "No deploy scans yet \u00c3\u00a2\u00e2\u201a\u00ac\u00e2\u20ac\u009d say 'deploy scan' in Telegram"};
