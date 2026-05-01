9adcf4a58cf7e88672d1386dff8a5d30d843633f
pdhruv22-hue <pdhruv22@gmail.com>
2026-04-30 22:58:41 -0400

    Update wealth-history-cumulative-data.js - goal analysis & wealth history

---
+++ wealth-history-cumulative-data.js
@@ -0,0 +1,21 @@
+window.WEALTH_HISTORY_DATA = {
+  "entries": [
+    {
+      "date": "2026-04-30",
+      "liquid_nw": 409293.72,
+      "merrill": 199530.82,
+      "crypto": 9762.9,
+      "bluestone": 200000,
+      "tsla_pct": 15.4,
+      "timestamp": "2026-04-30T22:51:17.957359"
+    }
+  ],
+  "first_entry": "2026-04-30",
+  "summary": {
+    "current_nw": 409293.72,
+    "target_nw": 1000000,
+    "gap": 590706.28,
+    "needed_cagr": 19.6,
+    "data_points": 1
+  }
+};
